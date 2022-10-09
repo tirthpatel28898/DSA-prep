5ad06177566a932617a5e61d49e4efcd29f90599
Tirth Patel <tirthpatel28898@gmail.com>
2022-10-08 23:41:47 -0400

    1. Two sum - HashMap

---
+++ Array/1_two_sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        prevMap = {} # val:index
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in prevMap:
+                return [prevMap[i], i]
+
+            prevMap[n] = i
+        
+        return
+
+s1 = Solution()
+
+print(s1.twoSum([1,2,3,1], 5))
