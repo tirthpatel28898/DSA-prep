5e74bcced0ae5397b6be3a0e9b9420d49daf2507
Tirth Patel <tirthpatel28898@gmail.com>
2022-10-09 23:42:06 -0400

    49. Group Anagrams - HashMap

---
+++ Array/49_group_anagrams.py
@@ -0,0 +1,13 @@
+import collections
+
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        ans = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
