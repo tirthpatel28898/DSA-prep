abdc3a984a954924670a989339fe446af7e8c39d
Tirth Patel <tirthpatel28898@gmail.com>
2022-10-09 23:55:46 -0400

    Update 1_two_sum.py

--- Array/1_two_sum.py
+++ Array/1_two_sum.py
@@ -4,14 +4,10 @@ class Solution:
 
         for i, n in enumerate(nums):
             diff = target - n
-
             if diff in prevMap:
                 return [prevMap[diff], i]
-
             prevMap[n] = i
-        
         return
 
 s1 = Solution()
-
 print(s1.twoSum([1,2,3,1], 5))
