c3744df13049e64bd26bd92ebc53e5bfebf68d76
Tirth Patel <tirthpatel28898@gmail.com>
2022-10-21 21:18:46 -0400

    268. Missing Number

---
+++ Bit Manipulation/268_missing_number.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: list[int]) -> int:
+        res = len(nums)
+        for i in range(len(nums)):
+            res += (i-nums[i])
+        return res
